98b438bd588ce191f285a1294df66dc16b2cea0c
csecec.1703023@gmail.com <csecec.1703023@gmail.com>
2021-06-10 12:52:48 +0530

    Second Commit

---
+++ Models/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppGitExercise2.Models
+{
+    [Table("Emp")]
+    public class Emp
+    {
+        [Key]
+        public int EID { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+
+}
